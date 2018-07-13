c7fa87b37810d63bbe35af01a55522cb4f4cb6fb
Sény Mbaye <senyr9@gmail.com>
2018-07-13 11:45:47 +0000

    demarrage de la version 4 avec spring boot et un theme html du nom de library

---
+++ src/main/java/org/drouss/apidrouss/ApidroussApplication.java
@@ -0,0 +1,12 @@
+package org.drouss.apidrouss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApidroussApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApidroussApplication.class, args);
+	}
+}
